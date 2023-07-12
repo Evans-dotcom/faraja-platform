caeec95d0af532df98120e1af5f75de06e48d4bc
Evans-dotcom <evaslangat@gmail.com>
2023-07-12 09:20:34 +0300

    initial commit

--- src/test/java/com/example/farajaplatform/FarajaPlatformApplicationTests.java
+++ src/test/java/com/example/farajaplatform/FarajaPlatformApplicationTests.java
@@ -2,9 +2,16 @@ package com.example.farajaplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes =FarajaPlatformApplicationTests.class )
 class FarajaPlatformApplicationTests {
+
+	@Configuration
+	static class TestConfig {
+	}
 
 	@Test
 	void contextLoads() {
